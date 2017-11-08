487e78859f0d261f49df4f09f60afaa378d50a54
Luis Francisco <lfquitian8@misena.edu.co>
2017-11-07 22:53:02 -0500

    07/11/2017  Backend
    proyecto desarrollado en android 3.0, el cual contiene las interfases graficas y los hilos encargados de consumir los servicios web

---
+++ proyectoFinal/app/src/main/java/com/example/usuario/proyectofinal/ContabilizadorActivity.java
@@ -0,0 +1,48 @@
+package com.example.usuario.proyectofinal;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ContabilizadorActivity extends AppCompatActivity implements SensorEventListener{
+
+    private TextView contador;
+    private SensorManager sensorM;
+    private Sensor s;
+    private int total = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contabilizador);
+        contador=(TextView)findViewById(R.id.tvContador);
+
+        sensorM=(SensorManager)getSystemService(SENSOR_SERVICE);
+
+        s=sensorM.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sensorM.registerListener(ContabilizadorActivity.this,s,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float valor=Float.parseFloat(String.valueOf(event.values[0]));
+
+        contador.setText(""+total);
+
+        if(valor==0){
+            total++;
+        }else{
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
